d3b06b1070ce4ad96086c1ed3ea9391f95b11452
Adam Ingor <adam.ingor1234@gmail.com>
2025-09-09 17:39:27 +0300

    Add turso db

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -3,6 +3,7 @@ import { createClient } from "@libsql/client";
 
 const client = createClient({
   url: process.env.DATABASE_URL || "",
+  authToken: process.env.DATABASE_TOKEN
 });
 
 export const db = drizzle({ client });

6e7cb83d70ca4b95eb3a00435f3cb924eb127386
Adam Ingor <adam.ingor1234@gmail.com>
2025-09-11 19:43:44 +0300

    Add latest changes

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -18,6 +18,16 @@ export const auth = betterAuth({
   },
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
+  advanced: {
+    cookiePrefix: "phone-case",
+    crossSubDomainCookies: {
+      enabled: true,
+    },
+    defaultCookieAttributes: {
+      secure: true,
+      sameSite: "none",    
+    },
+  },
 });
 
 
